150dad12ea394017820137a372d404d61988a651
aidominguezulhi <55478683+aidominguezulhi@users.noreply.github.com>
2022-02-11 15:50:56 +0100

    added all files

---
+++ PipeCliente/PipeCliente.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace pipeCliente
+{
+    class PipeCliente
+    {
+
+        static void Main(string[] args)
+        {
+            Process p;
+            StartServer(out p);
+            Task.Delay(1000).Wait();
+            //Preparar conexion del cliente
+            var client = new NamedPipeClientStream("PSP01_UD01_Pipes");
+            client.Connect();
+            StreamReader reader = new StreamReader(client);
+            StreamWriter writer = new StreamWriter(client);
+            var line = String.Empty;
+
+            while (true)
+            {
+                //solicitar el nombre del cuento
+                line = SolictarCuento(reader, writer);
+
+                //trabajamos la entrada y salida de datos
+                while (!line.Equals(" "))
+                {
+                    Console.WriteLine("{0}:", line);
+                    var line1 = Console.ReadLine();
+                    var palabra = "P " + line1;
+                    Console.WriteLine("Tubo cliente procesando datos: {0}", palabra);
+                    writer.WriteLine(palabra);
+                    writer.Flush();
+                    line = reader.ReadLine();
+                }
+                //mostramos el cuento completo
+                mostrarCuento(reader);
+                
+            }
+            //Parar pipes
+            if (p != null && !p.HasExited)
+            {
+                p.Kill();
+                p = null;
+            }
+        }
+
+        static Process StartServer(out Process p1)
+        {
+            // iniciar un proceso con el servidor y devolver
+            ProcessStartInfo info = new ProcessStartInfo(@"..\..\..\..\pipeServidor\bin\Release\net6.0\win-x64\pipeServidor.exe");
+            //ProcessStartInfo info = new ProcessStartInfo(@"C:\Users\Aitor\PSP7\TareaEvaluativa01\Solution1\PipeServidor\bin\Debug\net6.0\PipeServidor.exe");
+
+            // su valor por defecto el false, si se establece a true no se "crea" ventana
+            info.CreateNoWindow = false;
+            info.WindowStyle = ProcessWindowStyle.Normal;
+            // indica si se utiliza el cmd para lanzar el proceso
+            info.UseShellExecute = true;
+            p1 = Process.Start(info);
+            return p1;
+        }
+
+        //Sacamos por la consola lo que nos envia el servidor linea a linea hasta que nos llega un caracter vacio
+        private static void mostrarCuento(StreamReader reader)
+        {
+            Console.WriteLine("\n***************************");
+            Console.WriteLine("\n    el cuento creado es"    );
+            Console.WriteLine("\n***************************");
+            var line = reader.ReadLine();
+            while (!line.Equals("$$finDeFichero"))
+            {
+                Console.WriteLine(line);
+                line = reader.ReadLine();
+                
+            }
+        }
+
+        
+        private static string SolictarCuento(StreamReader reader, StreamWriter writer)
+        {
+            var line = string.Empty;
+            do
+            {
+                //Solicitamos elnombre del cuento por consola y le damos formato N + nombreFichero
+                Console.WriteLine("indica el nombre del cuento");
+                string cuento = Console.ReadLine();
+                cuento = "N " + cuento;
+
+                //Enviamos el dato al servidor
+                Console.WriteLine("Tubo Cliente procesando datos: {0}", cuento);
+                writer.WriteLine(cuento);
+                writer.Flush();
+
+
+                //Procesar huecos a rellenar. Recibimos una palabra desde el servidor y devolvemos otra en formato P + palabra
+                //Si entra un caracter vacio paramos pasamos a mostrar el cuento completo
+                line = reader.ReadLine();
+                Console.WriteLine("prueba {0}", line);
+            }
+            //mientras no exista el cuento nos quedamos bloqueados en este bucle
+            while (line == "El cuento no existe");
+            return line;
+        }
+            
+    }
+}
---
+++ PipeServidor/PipeServidor.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Threading.Tasks;
+
+namespace pipeServidor
+{
+    class PipeServidor
+    {
+        static void Main(string[] args)
+        {
+            var cuento = string.Empty;
+            
+            string s1 = string.Empty;
+            string s2 = string.Empty;
+            List<String> list = new List<String>();
+
+
+            try
+            {
+                //creamos la conexion
+                var server = new NamedPipeServerStream("PSP01_UD01_Pipes");
+                server.WaitForConnection();
+                Console.WriteLine("Conexión a servidor establecida.");
+                Console.WriteLine("Pipe Servidor esperando datos.");
+
+                //Creamos los buffer de entrada y de salida
+                StreamReader reader = new StreamReader(server);
+                StreamWriter writer = new StreamWriter(server);
+                while (true)
+                {
+                    //recibimos el cuento
+                    var line = reader.ReadLine();
+                    Console.WriteLine("Pipe Servidor recibiendo datos: '{0}'", line);
+                    //recibimos el nombre del cuento en formato N nombredelcuento y solo necesitamos el nombre del cuento,
+                    //eliminamos los dos primeros caracteres N y espacio
+                    cuento = line.Remove(0, 2);
+                    cuento = cuento + ".txt";
+                    
+                    Console.WriteLine("Apertura del fichero: {0}", cuento);
+                    File.WriteAllText(@"../../../../cuentos/resultado.txt", string.Empty);
+
+               
+                    //abrimos el fichero
+                    try
+                    {
+                        // abrimos el fichero para leer
+                        using (StreamReader sr = new StreamReader(@"../../../../cuentos/" + cuento))
+                        {
+                            Console.WriteLine("Fichero abierto. {0}", cuento);
+                            string fileLine;
+                            // leemos las lineas del fichero una a una
+                            while ((fileLine = sr.ReadLine()) != null)
+                            {
+                                list.Clear();
+                                Console.WriteLine("Tubo servidor procesando datos: {0}", fileLine);
+                                var words = fileLine.Split(' ');
+                                //procesar la linea del fichero
+                                foreach (var word in words)
+                                {
+                                    if (word.StartsWith("<"))
+                                    {
+                                        s1 = word.Remove(word.IndexOf("<"), 1);
+                                        s2 = s1.Remove(s1.IndexOf(">"), 1);
+                                        Console.WriteLine("Tubo servidor emitiendo datos: {0}", s2);
+                                        writer.WriteLine(s2);
+                                        writer.Flush();
+                                        var line1 = reader.ReadLine();
+                                        Console.WriteLine("Tubo Servidor recibiendo datos: '{0}'", line1);
+                                        var palabra = line1.Remove(0, 2);
+                                        list.Add(palabra);
+                                    }
+                                    else
+                                    {
+                                        list.Add(word);
+                                    }
+                                }
+
+                                var newLine = String.Join(" ", list.ToArray());
+                                using (StreamWriter sw = File.AppendText(@"../../../../cuentos/resultado.txt"))
+                                {
+                                    sw.WriteLine(newLine);
+                                }
+                            }
+                            writer.WriteLine(" ");
+                            writer.Flush();
+                            Console.WriteLine("Tubo cliente emitiendo datos cuento");
+                            extraerCuento(writer);
+                            writer.WriteLine("$$finDeFichero");
+                            writer.Flush();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        // Let the user know what went wrong.
+                        Console.WriteLine("El fichero no existe");
+                        Console.WriteLine(e.Message);
+                        writer.WriteLine("El cuento no existe");
+                        writer.Flush();
+                        
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}  Apangado servidor por error", e.Message);
+            }
+        }
+        private static void extraerCuento(StreamWriter writer)
+        {
+            string cuento = string.Empty;
+            string line = string.Empty;
+            try
+            {
+                StreamReader sr = new StreamReader(@"../../../../cuentos/resultado.txt");
+                line = sr.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    writer.WriteLine(line);
+                    writer.Flush();
+                    line = sr.ReadLine();
+                    
+                }
+                //close the file
+                
+                sr.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
